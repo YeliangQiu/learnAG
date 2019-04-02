4e192e1036517b2300dd906c46dbb595ddd8be95
仇烨亮 <qiuyeliang@MacBook-Pro.local>
2019-04-02 21:07:34 +0800

    20190402

---
+++ src/MultiplicationOfLargeIntergers/Main.java
@@ -0,0 +1,9 @@
+package MultiplicationOfLargeIntergers;
+
+/**
+ * @author : qiuyeliang
+ * create at:  2019/4/2  19:22
+ * @description: 有两个用字符串表示的非常大的大整数, 算出他们的乘积，也是用字符串表示。不能用系统自带的大整数类型。
+ */
+public class Main {
+}
